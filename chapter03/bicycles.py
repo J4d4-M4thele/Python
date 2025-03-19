4090e763b961b1dca531aa89b12197906bc1d69c
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-19 19:08:42 +0200

    update

--- chapter03/bicycles.py
+++ chapter03/bicycles.py
@@ -1,7 +1,11 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
+message = f"My first bicycle was a {bicycles[0].title()}"
+print(message)
 
 #accessing list elements
-print(bicycles[0])
+#print(bicycles[0])
 #printing with string methods
-print(bicycles[0].title())
+#print(bicycles[1].upper())
+#prints last item in list
+#print(bicycles[-1])
