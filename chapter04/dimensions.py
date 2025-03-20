21cca5740d2edd0a69d7ef126f7b20d29166dbda
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-20 19:15:22 +0200

    update

--- chapter04/dimensions.py
+++ chapter04/dimensions.py
@@ -1,11 +1,14 @@
 dimensions = (200, 50)
-print(dimensions[0])
-print(dimensions[1])
+# print(dimensions[0])
+# print(dimensions[1])
+
+# for d in dimensions:
+#     print(d)
 
 #throws an error
-dimensions[0] = 250
+#dimensions[0] = 250 #values can't be changed
 
-#looping through all values in a tuple
+# #looping through all values in a tuple
 print("Original dimensions:")
 for dimension in dimensions:
     print(dimension)
