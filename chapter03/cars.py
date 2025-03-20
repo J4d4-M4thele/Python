bdc8798fd9a500db2e3e14bfa2b9a4e937ea781f
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-20 17:19:40 +0200

    update

--- chapter03/cars.py
+++ chapter03/cars.py
@@ -1,25 +1,25 @@
 #sort() to sort list permanently
 cars = ['bmw', 'audi', 'toyota', 'subaru']
-cars.sort()
-print(cars)
+# cars.sort()
+# print(cars)
 #or the other way
-cars.sort(reverse=True)
-print(cars)
+# cars.sort(reverse=True)
+# print(cars)
 
 #sorted() to sort list temporarily
-print("Here is the original list:")
-print(cars)
+# print("Here is the original list:")
+# print(cars)
 
-print("\nHere is the sorted list:")
-print(sorted(cars))
+# print("\nHere is the sorted list:")
+# print(sorted(cars))
 
-print("\nHere is the original list again:")
-print(cars)
+# print("\nHere is the original list again:")
+# print(cars)
 
 #printing list in reverse order
-cars.reverse()
-print(cars)
+# cars.reverse()
+# print(cars)
 
 #finding list length
-cars = ['bmw', 'audi', 'toyota', 'subaru']
-len(cars)
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
