4438bcf29240d5c71e0d2a58478e0b76b03a99bf
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-20 19:06:13 +0200

    update

--- chapter04/foods.py
+++ chapter04/foods.py
@@ -1,14 +1,20 @@
 my_foods = ['pizza', 'falafel', 'carrot cake']
-friends_foods = my_foods[:]
+friends_foods = my_foods[:] # copies list items
 
-print("My favourite foods are:")
-print(my_foods)
+# print(f"My foods: {my_foods}")
+# print(f"My friend's foods: {friends_foods}")
+
+# print("My favourite foods are:")
+# print(my_foods)
 
 #this doesn't work
-friends_foods = my_foods
+#friends_foods = my_foods
 
 my_foods.append('cannoli')
 friends_foods.append('ice-cream')
 
-print("My friend's favourite foods are:")
-print(friends_foods)
+print(f"My foods: {my_foods}")
+print(f"My friend's foods: {friends_foods}")
+
+# print("My friend's favourite foods are:")
+# print(friends_foods)
