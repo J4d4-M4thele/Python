5dd5a163685ea7ff06b6f71de1f94bc693b6183d
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-20 18:45:54 +0200

    update

--- chapter04/square_numbers.py
+++ chapter04/square_numbers.py
@@ -1,6 +1,11 @@
 squares = []
-for value in range(1, 11):
-    square = value ** 2
-    squares.append(square)
+for value in range(1, 11): #1 - 10
+    squares.append(value ** 2)
 
 print(squares)    
+
+# cubes = []
+# for val in range(1, 11):
+#     cubes.append(val ** 3)
+
+# print(cubes)
