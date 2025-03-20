7051d9cf88feebab6017af26c4018b2abdebc2ad
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-20 18:39:16 +0200

    update

--- chapter04/first_numbers.py
+++ chapter04/first_numbers.py
@@ -1,9 +1,9 @@
 #range() function
-for value in range(1, 5):
-    print(value) #prints 1 to 4
+# for value in range(1, 5):
+#     print(value) #prints 1 to 4
 
-for value in range(1, 6):
-    print(value) #prints 1 to 5   
+# for value in range(1, 6):
+#     print(value) #prints 1 to 5   
 
 #making a list of numbers
 numbers = list(range(1,6))
