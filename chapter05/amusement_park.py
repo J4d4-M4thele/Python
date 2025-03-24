f93077adf783dce4355a8802ab68b9b48760b59c
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-24 19:11:31 +0200

    update

--- chapter05/amusement_park.py
+++ chapter05/amusement_park.py
@@ -1,11 +1,11 @@
-age = 12
+age = 70
 #multiple print statements
-if age < 4:
-    print("Your admission cost is $0.")
-elif age < 18:
-    print("Your admission cost is $25.")
-else:
-    print("Your admission cost is $40.")    
+# if age < 4:
+#     print("Your admission cost is $0.")
+# elif age < 18:
+#     print("Your admission cost is $25.")
+# else:
+#     print("Your admission cost is $40.")    
 
 #single print statement
 if age < 4:
@@ -15,9 +15,9 @@ elif age < 18:
 elif age < 65:
     price = 40
     #omitting else block
-    # elif age >= 65:
-    # price = 20 
-else:
-    price = 20
+elif age >= 65:
+    price = 20 
+# else:
+#     price = 20
 
-print("Your admission cost is ${price}.")       
+print(f"Your admission cost is ${price}.")       
