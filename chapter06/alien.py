f6cbb057431e96f498b727d4c9bffeae10b17aa7
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-24 18:02:48 +0200

    update

--- chapter06/alien.py
+++ chapter06/alien.py
@@ -17,3 +17,10 @@ alien_0['color'] = 'green'
 alien_0['points'] = 5
 
 print(alien_0)
+
+#modifying values in a dictionary
+alien_0 = {'color': 'green'}
+print(f"The alien is {alien_0['color']}.")
+
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}.")
