c48b3ed974a4df67d9b58c9d8b33c4ec2a205191
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-25 15:49:55 +0200

    update

--- chapter06/favourite_languages.py
+++ chapter06/favourite_languages.py
@@ -8,3 +8,33 @@ favorite_languages = {
 
 language = favorite_languages['sarah'].title()
 print(f"Sarah's favorite language is {language}.")
+
+# looping through all key-value pairs
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.")
+
+# looping through all keys (default behaviour of loop)
+for name in favorite_languages.keys(): 
+    print(name.title())
+
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}.")
+    if name in friends:
+        language = favorite_languages[name].title()
+    print(f"\t{name.title()}, I see you love {language}!")
+
+#lists inside of dictionaries
+favorite_languages = {
+'jen': ['python', 'rust'],
+'sarah': ['c'],
+'edward': ['rust', 'go'],
+'phil': ['python', 'haskell'],
+}
+for name, languages in favorite_languages.items():
+    print(f"\n{name.title()}'s favorite languages are:")
+    for language in languages:
+        print(f"\t{language.title()}")
