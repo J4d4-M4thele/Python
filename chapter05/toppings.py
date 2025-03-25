00e433802c89a12e1fdea91cd0ff98d4b134bb28
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-25 08:34:36 +0200

    update

--- chapter05/toppings.py
+++ chapter05/toppings.py
@@ -37,13 +37,13 @@
 #     print("Are you sure you want a plain pizza?")
 
 # #using multiple lists
-# available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
-# requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
 
-# for requested_topping in requested_toppings:
-#     if requested_topping in available_toppings:
-#         print(f"Adding {requested_topping}.")
-#     else:
-#         print(f"Sorry, we don't have {requested_topping}.")
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
 
-# print("\nFinished making your pizza!")
+print("\nFinished making your pizza!")
