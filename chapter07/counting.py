b560396127d338dfc4638e00ea84c2dbd5f27ea7
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-26 14:52:50 +0200

    first commit

---
+++ chapter07/counting.py
@@ -0,0 +1,14 @@
+current_number = 0
+
+# using continue in a loop 
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue #skips over even numbers
+    print(current_number)
+
+# infinite loop
+x = 1
+while x <= 5:
+    print(x)
+    x += 1 #will loop infinitely once this line is gone 
