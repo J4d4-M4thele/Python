a9c282ee50b46e938c7e893405e8104f285f4711
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-26 14:52:37 +0200

    update

--- chapter07/parrot.py
+++ chapter07/parrot.py
@@ -1,2 +1,20 @@
 message = input("Tell me something, and I'll repeat it back to you: ")
 print(message)
+
+# letting user quit when they want to 
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+message = ""
+
+while message != 'quit':
+    message = input(prompt)
+    print(message)
+
+# using a flag
+active = True
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else:
+        print(message) 
