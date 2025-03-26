73710be37d97579a9ab829e827b102a6cd7daeb8
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-26 17:26:30 +0200

    first commit

---
+++ chapter07/pets.py
@@ -0,0 +1,8 @@
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+
+# removes all instances of a specific value
+while 'cat' in pets:
+    pets.remove('cat')
+
+print(pets)
