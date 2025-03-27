f7b6bbd5658d78be3acd81bcdbb35262ed152434
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-27 16:59:59 +0200

    update

--- chapter08/pets.py
+++ chapter08/pets.py
@@ -18,3 +18,16 @@ def describe_pet(pet_name, animal_type='dog'):
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
 describe_pet(pet_name='willie')
+
+# equivalent function calls
+# A dog named Willie.
+describe_pet('willie')
+describe_pet(pet_name='willie')
+
+# A hamster named Harry.
+describe_pet('harry', 'hamster')
+describe_pet(pet_name='harry', animal_type='hamster')
+describe_pet(animal_type='hamster', pet_name='harry')
+
+# argument errors
+describe_pet()
