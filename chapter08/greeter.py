88d85f1ba9ee63fa24bad299159a4dd23b6263d9
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-27 16:33:05 +0200

    first commit

---
+++ chapter08/greeter.py
@@ -0,0 +1,12 @@
+def greet_user():
+    """Display a simple greeting"""
+    print("Hello!")
+
+greet_user()
+
+# passing infromation into a function
+def greet_user(username):
+    """Display a simple greeting"""
+    print(f"Hello {username}!")
+
+greet_user("Jada")
