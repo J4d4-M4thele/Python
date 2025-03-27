57574879cc2ea8250b472cbf5335f19aa42be605
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-27 17:14:17 +0200

    update

--- chapter08/formatted_name.py
+++ chapter08/formatted_name.py
@@ -19,3 +19,26 @@ musician = get_formatted_name('jimi', 'hendrix')
 print(musician)
 musician = get_formatted_name('john', 'hooker', 'lee')
 print(musician)
+
+# using functions with a while loop
+# This is an infinite loop!
+while True:
+    print("\nPlease tell me your name:")
+    f_name = input("First name: ")
+    l_name = input("Last name: ")
+    
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
+
+# preventing infinite loop
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+        l_name = input("Last name: ")
+    if l_name == 'q':
+        break
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")    
--- chapter08/greeter.py
+++ chapter08/greeter.py
@@ -4,9 +4,19 @@ def greet_user():
 
 greet_user()
 
-# passing infromation into a function
+# passing information into a function
 def greet_user(username):
     """Display a simple greeting"""
     print(f"Hello {username}!")
 
 greet_user("Jada")
+
+# passing a list
+def greet_users(names):
+    """Print a simple greeting to each user in the list."""
+    for name in names:
+        msg = f"Hello, {name.title()}!"
+        print(msg)
+        
+usernames = ['hannah', 'ty', 'margot']
+greet_users(usernames)
