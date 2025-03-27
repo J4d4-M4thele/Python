50fc0137b126dc761bae36ecbdb36f47e7f6540a
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-27 16:50:13 +0200

    update

---
+++ chapter08/pets.py
@@ -0,0 +1,20 @@
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet("tortoise", "junior")    
+# multiple functions
+describe_pet("dog", "tyson")
+
+# keyboard arguments
+describe_pet(animal_type='tortoise', pet_name='junior')
+describe_pet(animal_type='dog', pet_name='tyson')
+
+# default values
+def describe_pet(pet_name, animal_type='dog'):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet(pet_name='willie')
