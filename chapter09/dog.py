86c12e77ccdf429517ceb7805c41ec46df12934b
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-31 08:31:03 +0200

    update

--- chapter09/dog.py
+++ chapter09/dog.py
@@ -1,3 +1,4 @@
+# class declaration
 class Dog:
     """A simple dog model"""
     def __init__(self, name, age):
@@ -11,4 +12,20 @@ class Dog:
 
     def roll_over (self):
         """Simulate dog rolling over in response to command"""
-        print(f"{self.name} rolled over!")    
+        print(f"{self.name} rolled over!")  
+
+# creating an instance of the class
+my_dog = Dog('Willie', 6)
+your_dog = Dog('Lucy', 3)
+
+# accessing attributes
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+
+# calling methods
+my_dog.sit()
+my_dog.roll_over()
+
+print(f"\nYour dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
