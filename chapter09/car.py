d61ebb1eb72f61a9cc116d0a1c4befe3f3ac59f2
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-03-31 11:42:27 +0200

    update

--- chapter09/car.py
+++ chapter09/car.py
@@ -25,6 +25,28 @@ class Car:
         else: 
             print("You can't roll back the odometer!")    
 
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+class Battery:
+    """modelling an electric car battery"""
+    def __init__(self, battery_size = 40):
+        self.battery_size = battery_size 
+
+    def describe_battery(self):
+        """Prints statement describing battery"""
+        print(f"This car has a {self.battery_size}-kwh battery.")                       
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+            
+        print(f"This car can go about {range} miles on a full charge.")
+
 #creating class instance
 my_new_car = Car('audi', 'a4', 2024)
 print(my_new_car.get_descriptive_name())
@@ -36,4 +58,12 @@ my_new_car.read_odometer()
 
 my_new_car.update_odometer(23)
 
+# incrementing attribute's value through a method
+my_used_car = Car('subaru', 'outback', 2019)
+print(my_used_car.get_descriptive_name())
 
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
