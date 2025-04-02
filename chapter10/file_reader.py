3a47626c151d7774f9b017f7fcbc50557b437d61
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-04-02 09:47:51 +0200

    update

--- chapter10/file_reader.py
+++ chapter10/file_reader.py
@@ -10,10 +10,4 @@ contents = path.read_text()
 lines = contents.splitlines()
 for line in lines:
     print(line)
-
-# working with file contents
-pi_string = ''
-for line in lines:
-    pi_string += line.lstrip()
-print(f"PI: {pi_string}")
-print(f"PI length: {len(pi_string)}") 
+ 
