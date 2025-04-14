01fdfb939545465275c18d5b93a1f30ce10b28c4
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-04-14 16:21:20 +0200

    first commit

---
+++ chapter10/alice.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+path = Path('alice.txt')
+contents = path.read_text(encoding='utf-8')
+
+try:
+    contents = path.read_text(encoding='utf-8')
+except FileNotFoundError:
+    print("Sorry, the file alice.txt doesn't exist.") 
+else:
+    #Count approximate no. of words in file
+    words = contents.split()
+    num_words = len(words)
+    print(f"The file {path} has about {num_words} words.")    
