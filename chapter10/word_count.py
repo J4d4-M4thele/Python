88e24545fa66893e076be3cb396cbd4266e6e40c
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-04-14 16:29:47 +0200

    update

--- chapter10/word_count.py
+++ chapter10/word_count.py
@@ -6,15 +6,16 @@ def count_words(path):
     try:
         contents = path.read_text(encoding='utf-8')
     except FileNotFoundError:
-        pass
+        print(f"Sorry, the file {path} does not exist.")
+        # pass
     else:
         # Count the approximate number of words in the file:
         words = contents.split()
         num_words = len(words)
         print(f"The file {path} has about {num_words} words.")
 
-filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt',
-        'little_women.txt']
-for filename in filenames:
-    path = Path(filename)
-    count_words(path)
+# filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt',
+#         'little_women.txt']
+# for filename in filenames:
+#     path = Path(filename)
+#     count_words(path)
