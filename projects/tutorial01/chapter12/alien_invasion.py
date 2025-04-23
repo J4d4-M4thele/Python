2b2dedfe1f55f2942036707c3b553f34de3b20ec
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-04-23 13:14:02 +0200

    first commit

---
+++ projects/tutorial01/chapter12/alien_invasion.py
@@ -0,0 +1,36 @@
+import sys
+import pygame
+
+class AlienInvasion: 
+    """Class manages game assets and behaviour"""
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+        # set background colour
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # redraw the screen during each pass of loop
+            self.screen.fill(self.bg_color)
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+            # pygame will try loop 60 times per second  
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()            
